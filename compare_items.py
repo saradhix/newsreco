f47acb973ae83eb18724c8b86a86ecf26cf33a88
Saradhi <saradhix@yahoo.com>
2016-06-15 10:53:26 -0700

    Added trigrams to compare_items

--- compare_items.py
+++ compare_items.py
@@ -41,6 +41,7 @@ for i in range(len(doclist)):
 #element in the tuple
 
 top_similar = sorted(similarity_matrix, key = lambda x:x[2], reverse=True)[0:top_matches_count]
+print len(top_similar)
 print top_similar
 
 print "Printing pairs"
@@ -58,9 +59,13 @@ for item in top_similar:
   print set(doclist[item[0]]) & set(doclist[item[1]])
   bigrams1 = mylib.make_bigrams(doc1[0])
   bigrams2 = mylib.make_bigrams(doc2[0])
+  trigrams1 = mylib.make_trigrams(doc1[0])
+  trigrams2 = mylib.make_trigrams(doc2[0])
   js_bigram_similarity = mylib.jaccard_similarity(bigrams1, bigrams2)
-  print "JS_bigram=", js_bigram_similarity
-  print "Time difference=", (int(doc1[1])-int(doc2[1]))
+  js_trigram_similarity = mylib.jaccard_similarity(trigrams1, trigrams2)
+  print ""
+  print "JS_bigram=", js_bigram_similarity,"JS_trigram=", js_trigram_similarity
+  print "Time difference=", abs(int(doc1[1])-int(doc2[1]))/3600000, "t1=", doc1[1], "t2=",doc2[1]
   print set(bigrams1) & set(bigrams2)
   print "=="*40
   print "=="*40
